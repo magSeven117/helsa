838a05f3a8802a2e9f2acd4037d6532ea96cf972
Jose Veliz <ducen29@gmail.com>
2024-11-03 22:12:05 -0400

    Top bar searcher

--- src/app/(app)/components/shared/top-bar/top-bar.tsx
+++ src/app/(app)/components/shared/top-bar/top-bar.tsx
@@ -14,32 +14,24 @@ import {
 import { Input } from '@/libs/shadcn-ui/components/input';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/libs/shadcn-ui/components/tabs';
 import { useClerk } from '@clerk/nextjs';
-import {
-  Bell,
-  Command,
-  CreditCard,
-  Inbox,
-  Loader2,
-  LogOut,
-  Settings,
-  Sparkles,
-  User
-} from 'lucide-react';
+import { Bell, Command, CreditCard, Inbox, Loader2, LogOut, Search, Settings, Sparkles, User, X } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { SidebarTrigger } from '../side-bar/sidabar-trigger';
 
 const TopBar = () => {
-  const { user } = useClerk()
+  const { user } = useClerk();
   return (
     <div className="flex w-full justify-between items-center  px-8 py-5 border-b">
-      <div className="flex items-center gap-3">
+      <div className="flex w-1/2 items-center gap-3">
         <SidebarTrigger />
-        <TopButton />
+        <Searcher />
       </div>
-      <div className='flex items-center gap-3'>
+      <div className="flex items-center gap-3">
+        <AIButton />
+        <CommandButton />
         <NotificationButton />
-        <ProfileButton user={user}/>
+        <ProfileButton user={user} />
       </div>
     </div>
   );
@@ -57,6 +49,13 @@ const Searcher = () => {
   const searchResults = async (search: string) => {
     await new Promise((resolve) => setTimeout(resolve, 500));
     setResults([
+      {
+        title: 'Jose Veliz',
+        description: 'Diabetes type 2',
+        age: 25,
+        last_consult: '2021-10-10',
+        image: 'https://avatars.githubusercontent.com/u/1403241?v=4',
+      },
       {
         title: 'Jose Veliz',
         description: 'Diabetes type 2',
@@ -83,38 +82,57 @@ const Searcher = () => {
     };
   }, [term]);
   return (
-    <div className="w-full rounded-full flex flex-col relative items-center gap-2 max-md:w-1/2">
-      <Input
-        className="border border-secondary bg-background outline-none text-secondary-foreground rounded-full box-border w-4/5 focus-visible:ring-transparent"
-        placeholder="Search"
-        onChange={handleChange}
-      />
+    <div className="rounded-full flex-1 flex relative items-center gap-2">
+      <div className="w-full pr-10 relative">
+        <Input
+          className="outline-none text-secondary-foreground rounded-none box-border focus-visible:ring-transparent"
+          placeholder="Buscar"
+          value={term}
+          onChange={handleChange}
+        />
+        {!isSearching && term && results.length > 0 ? (
+          <div
+            className="absolute z-20 right-[46px] top-[7px] bg-sidebar p-1 flex justify-center items-center border rounded-none text-xs cursor-pointer"
+            onClick={() => setTerm('')}
+          >
+            <X className="size-4" />
+          </div>
+        ) : (
+          <div className="absolute z-20 right-[46px] top-[7px] bg-sidebar p-1 flex justify-center items-center border rounded-none text-xs">
+            <Search className="size-4" />
+          </div>
+        )}
+      </div>
       {isSearching && (
-        <div className="z-50 bg-background w-4/5 h-[250px] rounded shadow border left-[50px] top-[50px]">
-          <div className="flex justify-center items-center h-full">
-            <Loader2 className="h-[50px] w-[50px] text-primary animate-spin"></Loader2>
+        <div className="absolute w-full pr-10 z-50 bg-background left-0 top-[50px]">
+          <div className=" bg-background w-full h-[250px] rounded-none shadow border">
+            <div className="flex justify-center items-center h-full">
+              <Loader2 className="h-[50px] w-[50px] text-primary animate-spin"></Loader2>
+            </div>
           </div>
         </div>
       )}
       {!isSearching && term && results.length > 0 && (
-        <div className="z-50 bg-background w-4/5 h-[250px] rounded shadow border left-[50px] top-[50px]">
-          {results.map((result, index) => (
-            <div key={index} className="flex gap-2 p-2 cursor-pointer hover:bg-border">
-              <img src={result.image} alt={result.title} className="h-[50px] w-[50px] rounded-full" />
-              <div className="flex flex-col justify-center">
-                <div className="font-bold text-[1rem]">
-                  {result.title} - <span className="font-bold">{result.age} years</span>
-                </div>
-                <div className="text-xs">
-                  {result.description} - <span className="italic">last consult {result.last_consult}</span>
+        <div className="absolute w-full pr-10 z-50 bg-background left-0 top-[50px]">
+          <div className="w-full h-[250px] rounded-none shadow border p-2 space-y-2">
+            {results.map((result, index) => (
+              <div key={index} className="flex gap-2 p-2 cursor-pointer hover:bg-border border">
+                <img src={result.image} alt={result.title} className="h-[50px] w-[50px] rounded-full" />
+                <div className="flex flex-col justify-center">
+                  <div className="font-bold text-[1rem]">
+                    {result.title} - <span className="font-bold">{result.age} years</span>
+                  </div>
+                  <div className="text-xs">
+                    {result.description} - <span className="italic">last consult {result.last_consult}</span>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       )}
       {!isSearching && term && results.length === 0 && (
-        <div className="z-50 bg-background w-4/5 h-[250px] rounded shadow border left-[50px] top-[50px]">
+        <div className="absolute z-50 bg-background w-4/5 h-[250px] rounded shadow border left-[50px] top-[50px]">
           <div className="flex justify-center items-center h-full text-xl font-bold">No results found</div>
         </div>
       )}
@@ -122,10 +140,19 @@ const Searcher = () => {
   );
 };
 
-const TopButton = () => {
+const AIButton = () => {
   return (
     <div className="flex justify-start items-center gap-3 text-muted-foreground hover:text-foreground cursor-pointer text-sm">
-      <p className="font-light">Pregúntale a Helsa por cualquier cosa</p>
+      <span className="bg-sidebar p-1 flex justify-center items-center border rounded-none text-xs">
+        <Sparkles className="size-4 mr-2" /> {' Helsa IA'}
+      </span>
+    </div>
+  );
+};
+
+const CommandButton = () => {
+  return (
+    <div className="flex justify-start items-center gap-3 text-muted-foreground hover:text-foreground cursor-pointer text-sm">
       <span className="bg-sidebar p-1 flex justify-center items-center border rounded-none text-xs">
         <Command className="size-4" /> {' + K'}
       </span>
@@ -191,14 +218,15 @@ const ProfileButton = ({ user }: { user: any }) => {
           className="w-10 h-10 rounded-full p-0 outline-none border-none focus-visible:outline-none focus:outline-none"
         >
           <Avatar className="h-10 w-10 rounded-full">
-            <AvatarImage src={user?.imageUrl || ''} alt={user?.fullName || '' }  className='object-contain'/>
+            <AvatarImage src={user?.imageUrl || ''} alt={user?.fullName || ''} className="object-contain" />
             <AvatarFallback className="rounded-lg">CN</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent
         className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-none"
-        align="end" sideOffset={8}
+        align="end"
+        sideOffset={8}
       >
         <DropdownMenuLabel className="p-0 font-normal">
           <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
@@ -211,28 +239,28 @@ const ProfileButton = ({ user }: { user: any }) => {
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
           <DropdownMenuItem className="gap-2 text-sm rounded-xs cursor-pointer">
-            <Sparkles className='size-4'/>
+            <Sparkles className="size-4" />
             Upgrade to Pro
           </DropdownMenuItem>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
           <DropdownMenuItem className="gap-2 text-sm rounded-xs cursor-pointer" onClick={() => router.push('/profile')}>
-            <User className='size-4'/>
+            <User className="size-4" />
             Profile
           </DropdownMenuItem>
           <DropdownMenuItem className="gap-2 text-sm rounded-xs cursor-pointer">
-            <CreditCard className='size-4'/>
+            <CreditCard className="size-4" />
             Billing
           </DropdownMenuItem>
           <DropdownMenuItem className="gap-2 text-sm rounded-xs cursor-pointer">
-            <Bell className='size-4'/>
+            <Bell className="size-4" />
             Notifications
           </DropdownMenuItem>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
         <DropdownMenuItem className="gap-2 text-sm rounded-xs cursor-pointer" onClick={onClick}>
-          <LogOut className='size-4'/>
+          <LogOut className="size-4" />
           Log out
         </DropdownMenuItem>
       </DropdownMenuContent>
