2b3e63f7334272abd4a88c1f0266c3b539f4e81d
Jose Veliz <ducen29@gmail.com>
2024-11-07 21:52:50 -0400

    Education level section feat

--- src/app/(app)/components/profile/patient-info/patient-info.tsx
+++ src/app/(app)/components/profile/patient-info/patient-info.tsx
@@ -1,6 +1,7 @@
 import { Patient } from "@/modules/patient/domain/patient";
 import { Primitives } from "@/modules/shared/domain/types/primitives";
 import { CivilStatusSection } from "./sections/civil-status-section";
+import { EducationLevelSection } from "./sections/education-level-section";
 import { OccupationSection } from "./sections/occupation-section";
 
 const PatientInfo = ({ patient }: { patient: Partial<Primitives<Patient>> }) => {
@@ -8,6 +9,7 @@ const PatientInfo = ({ patient }: { patient: Partial<Primitives<Patient>> }) => 
     <div className="flex flex-col w-full gap-10">
       <CivilStatusSection civilStatus={patient.demographic.civilStatus as any} id={patient.id}/>
       <OccupationSection occupation={patient.demographic.occupation} id={patient.id} />
+      <EducationLevelSection educationLevel={patient.demographic.educativeLevel as any} id={patient.id}/>
     </div>
   );
 }
