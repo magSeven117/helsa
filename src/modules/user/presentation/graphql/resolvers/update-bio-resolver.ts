b8b0bdd95690dd16d757cbbff9d9f9dfa8257d0b
Jose Veliz <ducen29@gmail.com>
2024-11-24 19:07:45 -0400

    Remove clerk

--- src/modules/user/presentation/graphql/resolvers/update-bio-resolver.ts
+++ src/modules/user/presentation/graphql/resolvers/update-bio-resolver.ts
@@ -1,15 +1,9 @@
 import { InternalError } from '@/modules/shared/domain/core/errors/internal-error';
-import { auth, clerkClient } from '@clerk/nextjs/server';
 
-export const UpdateBioResolver = async (context, input) => {
+export const UpdateBioResolver = async (_: unknown, input: { bio: string }) => {
   try {
-    const user = auth();
     const { bio } = input;
-    await clerkClient.users.updateUserMetadata(user.userId, {
-      publicMetadata: {
-        bio,
-      },
-    });
+    console.log(bio);
   } catch (error) {
     console.error(error);
     throw new InternalError('An error occurred. Please try again.');
