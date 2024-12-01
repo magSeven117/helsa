99d42d6e2a9f29390c2f6698fcc2a4896f17cf48
Jose Veliz <ducen29@gmail.com>
2024-12-01 16:47:57 -0400

    Fix build

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ import { fileURLToPath } from 'node:url';
 const jiti = createJiti(fileURLToPath(import.meta.url));
 jiti.import('./src/modules/shared/infrastructure/config/env');
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['@simplewebauthn/server', '@trigger.dev/sdk'],
+  },
+};
 
 export default nextConfig;
--- src/modules/shared/domain/core/aggregate.ts
+++ src/modules/shared/domain/core/aggregate.ts
@@ -3,20 +3,20 @@ import { DateValueObject } from './value-object';
 import { Uuid } from './value-objects/uuid';
 
 export abstract class Aggregate {
-  private domainEvents: Array<DomainEvent>;
+  private domainEvents: Array<DomainEvent<any>>;
   abstract toPrimitives(): any;
   static fromPrimitives: (params: any) => Aggregate;
   constructor(public id: Uuid, public createdAt: DateValueObject, public updatedAt: DateValueObject) {
     this.domainEvents = [];
   }
 
-  public pullDomainEvents(): Array<DomainEvent> {
+  public pullDomainEvents(): Array<DomainEvent<any>> {
     const domainEvents = this.domainEvents.slice();
     this.domainEvents = [];
     return domainEvents;
   }
 
-  public record(event: DomainEvent): void {
+  public record(event: DomainEvent<any>): void {
     this.domainEvents.push(event);
   }
 }
