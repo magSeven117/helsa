e138a01f5bf00681f4da9d7a92fff408a1d943e0
Jose Veliz <ducen29@gmail.com>
2024-12-04 11:37:24 -0400

    using ts

--- src/modules/appointment/application/get-doctor-appointments.ts
+++ src/modules/appointment/application/get-doctor-appointments.ts
@@ -1,12 +1,13 @@
 import { AppointmentCriteria } from '../domain/appointment-criteria';
 import { AppointmentRepository } from '../domain/appointment-repository';
+import { AppointmentStatusEnum } from '../domain/status';
 
 export class GetDoctorAppointments {
   constructor(private appointmentRepository: AppointmentRepository) {}
 
   async run(doctorId: string) {
     const criteria = AppointmentCriteria.searchByDoctorId(doctorId).and(
-      AppointmentCriteria.searchByStatus('SCHEDULED')
+      AppointmentCriteria.searchByStatus(AppointmentStatusEnum.SCHEDULED)
     );
     const appointments = await this.appointmentRepository.search(criteria);
     return appointments;
