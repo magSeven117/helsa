4cf3a2ce0f226884bd99f501fb4a22ef96765698
Jose Veliz <ducen29@gmail.com>
2025-01-05 15:04:12 -0400

    testing dynamic ssr false

--- src/apps/app/src/app/(auth)/sign-up/page.tsx
+++ src/apps/app/src/app/(auth)/sign-up/page.tsx
@@ -1,5 +1,7 @@
 import dynamic from 'next/dynamic';
-const SignUpForm = dynamic(() => import('@/src/components/auth/sign-up-form').then((m) => m.default));
+const SignUpForm = dynamic(() => import('@/src/components/auth/sign-up-form').then((m) => m.default), {
+  ssr: false,
+});
 export default function Page({ params }: { params: { userId: string } }) {
   return <SignUpForm></SignUpForm>;
 }
