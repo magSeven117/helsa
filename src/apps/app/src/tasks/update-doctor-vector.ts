478bf88569eb0da36e4da27f6ed72cf232aa2b67
Jose Veliz <ducen29@gmail.com>
2025-01-05 14:12:05 -0400

    task

--- src/apps/app/src/tasks/update-doctor-vector.ts
+++ src/apps/app/src/tasks/update-doctor-vector.ts
@@ -1,3 +1,4 @@
+import { database } from '@helsa/database';
 import { DomainEventPrimitives } from '@helsa/ddd/core/domain-event';
 import { GetDoctorAppointments } from '@helsa/engine/appointment/application/get-doctor-appointments';
 import { PrismaAppointmentRepository } from '@helsa/engine/appointment/infrastructure/persistence/prisma-appointment-repository';
@@ -5,7 +6,6 @@ import { UpdateIndexStore } from '@helsa/engine/doctor/application/services/upda
 import { DoctorEventData } from '@helsa/engine/doctor/domain/events/doctor-event-data';
 import { PrismaDoctorRepository } from '@helsa/engine/doctor/infrastructure/persistence/prisma-doctor-repository';
 import { PrismaDoctorSearcher } from '@helsa/engine/doctor/infrastructure/persistence/prisma-doctor-searcher';
-import { db } from '@helsa/engine/shared/infrastructure/persistence/prisma/prisma-connection';
 import { GetUser } from '@helsa/engine/user/application/get-user';
 import { PrismaUserRepository } from '@helsa/engine/user/infrastructure/prisma-user-repository';
 import { task } from '@trigger.dev/sdk/v3';
@@ -16,10 +16,10 @@ export const UpdateDoctorVectorTask = task({
   run: async (payload: DomainEventPrimitives<DoctorEventData>) => {
     const { data } = payload;
     const service = new UpdateIndexStore(
-      new PrismaDoctorRepository(db),
-      new PrismaDoctorSearcher(db),
-      new GetUser(new PrismaUserRepository(db)),
-      new GetDoctorAppointments(new PrismaAppointmentRepository(db)),
+      new PrismaDoctorRepository(database),
+      new PrismaDoctorSearcher(database),
+      new GetUser(new PrismaUserRepository(database)),
+      new GetDoctorAppointments(new PrismaAppointmentRepository(database)),
     );
 
     await service.run(data.doctorId);
