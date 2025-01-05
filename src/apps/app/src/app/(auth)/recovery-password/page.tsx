ac43447cdef26af0abe931ab43d2a7010de6c873
Jose Veliz <ducen29@gmail.com>
2025-01-05 15:23:16 -0400

    testing dynamic ssr false

--- src/apps/app/src/app/(auth)/recovery-password/page.tsx
+++ src/apps/app/src/app/(auth)/recovery-password/page.tsx
@@ -1,7 +1,8 @@
-'use client';
+import { getCurrentUser } from '@/src/actions/user/get-current-user';
 import RecoveryPasswordForm from '@/src/components/auth/recovery-password-form';
 
-const Page = () => {
+const Page = async () => {
+  await getCurrentUser();
   return <RecoveryPasswordForm />;
 };
 
