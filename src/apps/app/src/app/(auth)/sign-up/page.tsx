e712466f4ae30dd16e28e7c796828ed2ef94803a
Jose Veliz <ducen29@gmail.com>
2025-01-05 15:01:17 -0400

    testing dynamic

--- src/apps/app/src/app/(auth)/sign-up/page.tsx
+++ src/apps/app/src/app/(auth)/sign-up/page.tsx
@@ -1,5 +1,5 @@
 import dynamic from 'next/dynamic';
-const SignUpForm = dynamic(() => import('@/src/components/auth/sign-up-form'));
+const SignUpForm = dynamic(() => import('@/src/components/auth/sign-up-form').then((m) => m.default));
 export default function Page({ params }: { params: { userId: string } }) {
   return <SignUpForm></SignUpForm>;
 }
