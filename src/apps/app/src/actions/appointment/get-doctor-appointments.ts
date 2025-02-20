dfd3e9242f409a97398b46bfc0f58de3bc8c55c4
Jose Veliz <ducen29@gmail.com>
2025-02-19 23:58:59 -0400

    fix types

--- src/apps/app/src/actions/appointment/get-doctor-appointments.ts
+++ src/apps/app/src/actions/appointment/get-doctor-appointments.ts
@@ -4,19 +4,18 @@ import { authActionClient } from '@helsa/actions';
 import { database } from '@helsa/database';
 import { GetDoctorAppointments } from '@helsa/engine/appointment/application/get-doctor-appointments';
 import { PrismaAppointmentRepository } from '@helsa/engine/appointment/infrastructure/persistence/prisma-appointment-repository';
-import { z } from 'zod';
-
-const schema = z.object({
-  doctorId: z.string(),
-});
+import { GetDoctor } from '@helsa/engine/doctor/application/services/get-doctor';
+import { PrismaDoctorRepository } from '@helsa/engine/doctor/infrastructure/persistence/prisma-doctor-repository';
 
 export const getDoctorAppointments = authActionClient
-  .schema(schema)
   .metadata({
     actionName: 'get-doctor-appointments',
   })
-  .action(async ({ parsedInput: { doctorId } }) => {
-    const service = new GetDoctorAppointments(new PrismaAppointmentRepository(database));
-    const response = await service.run(doctorId);
-    return response.getItems();
+  .action(async ({ ctx: { user } }) => {
+    const service = new GetDoctorAppointments(
+      new PrismaAppointmentRepository(database),
+      new GetDoctor(new PrismaDoctorRepository(database))
+    );
+    const response = await service.run(user.id, {});
+    return response.data;
   });
--- src/apps/app/src/tasks/update-doctor-vector.ts
+++ src/apps/app/src/tasks/update-doctor-vector.ts
@@ -2,6 +2,7 @@ import { database } from '@helsa/database';
 import { DomainEventPrimitives } from '@helsa/ddd/core/domain-event';
 import { GetDoctorAppointments } from '@helsa/engine/appointment/application/get-doctor-appointments';
 import { PrismaAppointmentRepository } from '@helsa/engine/appointment/infrastructure/persistence/prisma-appointment-repository';
+import { GetDoctor } from '@helsa/engine/doctor/application/services/get-doctor';
 import { UpdateIndexStore } from '@helsa/engine/doctor/application/services/update-index-store';
 import { DoctorEventData } from '@helsa/engine/doctor/domain/events/doctor-event-data';
 import { PrismaDoctorRepository } from '@helsa/engine/doctor/infrastructure/persistence/prisma-doctor-repository';
@@ -19,7 +20,10 @@ export const UpdateDoctorVectorTask = task({
       new PrismaDoctorRepository(database),
       new PrismaDoctorSearcher(database),
       new GetUser(new PrismaUserRepository(database)),
-      new GetDoctorAppointments(new PrismaAppointmentRepository(database)),
+      new GetDoctorAppointments(
+        new PrismaAppointmentRepository(database),
+        new GetDoctor(new PrismaDoctorRepository(database))
+      )
     );
 
     await service.run(data.doctorId);
