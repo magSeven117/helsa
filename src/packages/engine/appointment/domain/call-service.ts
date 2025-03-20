e6cfd078ae080cb883766cdc106d592e03d1e640
Jose Veliz <ducen29@gmail.com>
2025-03-19 23:44:55 -0400

    Fixing error

--- src/packages/engine/appointment/domain/call-service.ts
+++ src/packages/engine/appointment/domain/call-service.ts
@@ -1,6 +1,6 @@
-import { Doctor } from '@/src/doctor/domain/doctor';
-import { Patient } from '@/src/patient/domain/patient';
 import { Primitives } from '@helsa/ddd/types/primitives';
+import { Doctor } from '../../doctor/domain/doctor';
+import { Patient } from '../../patient/domain/patient';
 
 export interface CallService {
   createRoom(appointmentId: string, doctor: Primitives<Doctor>, patient: Primitives<Patient>): Promise<void>;
