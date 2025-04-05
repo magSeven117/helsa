ff8698bdb4265a30488649672e2f6e3d0cc7d0ef
Jose Veliz <ducen29@gmail.com>
2025-04-05 13:40:21 -0400

    Use mutations of react-query

--- src/apps/app/src/hooks/appointment/use-notes.ts
+++ src/apps/app/src/hooks/appointment/use-notes.ts
@@ -1,17 +1,21 @@
+import { useMutation } from '@tanstack/react-query';
+
 export const useNotes = () => {
-  const createNote = async ({ appointmentId, note }: { appointmentId: string; note: string }) => {
-    const response = await fetch(`/api/v1/appointment/${appointmentId}/notes`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ note }),
-    });
-    if (!response.ok) {
-      throw new Error('Error creating note');
-    }
-    return response.json();
-  };
+  const { mutateAsync: createNote } = useMutation({
+    mutationFn: async ({ appointmentId, note }: { appointmentId: string; note: string }) => {
+      const response = await fetch(`/api/v1/appointment/${appointmentId}/notes`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ note }),
+      });
+      if (!response.ok) {
+        throw new Error('Error creating note');
+      }
+      return response.json();
+    },
+  });
 
   return {
     createNote,
