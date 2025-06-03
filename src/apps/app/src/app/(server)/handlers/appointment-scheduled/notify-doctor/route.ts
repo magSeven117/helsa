2c0e0aa32a1e063f790514b97401a38891595657
Jose Veliz <ducen29@gmail.com>
2025-06-03 18:05:26 -0400

    Add cron

--- src/apps/app/src/app/(server)/handlers/appointment-scheduled/notify-doctor/route.ts
+++
@@ -1,3 +0,0 @@
-export const GET = async () => {
-  return new Response('This route is not meant to be accessed directly. Use POST to trigger the workflow.');
-};
--- src/apps/app/src/app/(server)/handlers/listeners.ts
+++
@@ -1,4 +0,0 @@
-export const listeners = {
-  'appointment-scheduled': ['update-doctor-vector', 'create-appointment-call-room'],
-  'user-entered-appointment': ['start-appointment'],
-};
