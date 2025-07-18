006dea573554a189c8cb8e5b6bb4b85cbadadcc0
Jose Veliz <ducen29@gmail.com>
2025-07-18 18:20:30 -0400

    feat: update Header component with new logo and improve navigation menu styles

--- src/apps/website/src/components/shared/header.tsx
+++ src/apps/website/src/components/shared/header.tsx
@@ -1,4 +1,5 @@
 'use client';
+import logoComplete from '@/src/assets/images/HELSA NUEVO BLANCO.png';
 import { useTheme } from '@helsa/ui';
 import { Button } from '@helsa/ui/components/button';
 import {
@@ -8,45 +9,75 @@ import {
   NavigationMenuList,
   navigationMenuTriggerStyle,
 } from '@helsa/ui/components/navigation-menu';
+import { cn } from '@helsa/ui/lib/utils';
 import { Moon, Sun } from 'lucide-react';
 import Link from 'next/link';
-import { HelsaCompleteLogo } from './helsa-icon';
 
 const Header = () => {
   const { setTheme } = useTheme();
   return (
-    <header className="sticky top-0 z-50 hidden  md:flex justify-center w-full bg-background h-[80px] min-[1700px]:h-[100px]">
-      <div className="w-[80%] min-[1700px]:w-2/3 flex items-center justify-between gap-10  lg:py-3 container relative  rounded-lg">
+    <header className="top-0 z-50 hidden  md:flex justify-center w-full bg-background h-[80px] min-[1700px]:h-[80px]">
+      <div className="w-[80%] min-[1700px]:w-2/3 px-6 mt-2 flex items-center justify-between gap-10  container relative  rounded-lg bg-foreground dark:bg-background dark:border border-border">
         <div className="flex items-center gap-4">
           <Link className="flex items-center gap-2" href="/">
-            <HelsaCompleteLogo className="object-contain" />
+            <img src={logoComplete.src} alt="" className="object-contain h-10" />
           </Link>
         </div>
 
         <NavigationMenu>
           <NavigationMenuList>
             <NavigationMenuItem>
-              <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+              <NavigationMenuLink
+                asChild
+                className={cn(
+                  navigationMenuTriggerStyle(),
+                  'bg-foreground dark:bg-background text-white hover:bg-brand-primary dark:hover:bg-brand-primary dark:text-white hover:text-white',
+                )}
+              >
                 <Link href="/about">Nosotros</Link>
               </NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+              <NavigationMenuLink
+                asChild
+                className={cn(
+                  navigationMenuTriggerStyle(),
+                  'bg-foreground dark:bg-background text-white hover:bg-brand-primary dark:hover:bg-brand-primary dark:text-white hover:text-white',
+                )}
+              >
                 <Link href="/professionals">Profesionales</Link>
               </NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+              <NavigationMenuLink
+                asChild
+                className={cn(
+                  navigationMenuTriggerStyle(),
+                  'bg-foreground dark:bg-background text-white hover:bg-brand-primary dark:hover:bg-brand-primary dark:text-white hover:text-white',
+                )}
+              >
                 <Link href="/patients">Pacientes</Link>
               </NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+              <NavigationMenuLink
+                asChild
+                className={cn(
+                  navigationMenuTriggerStyle(),
+                  'bg-foreground dark:bg-background text-white hover:bg-brand-primary dark:hover:bg-brand-primary dark:text-white hover:text-white',
+                )}
+              >
                 <Link href="/pricing">Precios</Link>
               </NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+              <NavigationMenuLink
+                asChild
+                className={cn(
+                  navigationMenuTriggerStyle(),
+                  'bg-foreground dark:bg-background text-white hover:bg-brand-primary dark:hover:bg-brand-primary dark:text-white hover:text-white',
+                )}
+              >
                 <Link href="/blog">Blog</Link>
               </NavigationMenuLink>
             </NavigationMenuItem>
@@ -57,11 +88,14 @@ const Header = () => {
             <Button variant={'outline'}>Entrar</Button>
           </Link>
           <Link href={'https://app.helsahealthcarte.com/sign-up'}>
-            <Button variant={'primary'}>Registrarse</Button>
+            <Button variant={'primary'} className="text-white">
+              Registrarse
+            </Button>
           </Link>
           <Button
-            className=""
+            className="text-brand-primary"
             size={'icon'}
+            variant={'ghost'}
             onClick={() => {
               setTheme((prev) => (prev === 'light' ? 'dark' : 'light'));
             }}
