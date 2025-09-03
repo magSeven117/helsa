bbb16ff3019fbadf5f5499b9334d2dd906fc0a8c
magSeven117 <65859907+magSeven117@users.noreply.github.com>
2025-09-03 15:07:25 -0500

    start_change_video_service

--- src/apps/app/src/app/(app)/(main)/schedule/@patient/page.tsx
+++ src/apps/app/src/app/(app)/(main)/schedule/@patient/page.tsx
@@ -1,5 +1,11 @@
+import { CalendarView } from '@/src/components/schedule/calendar-view';
+
 const Page = () => {
-  return <div className="flex w-full h-full px-5 py-7">doctor schedule</div>;
+  return (
+    <div className="flex w-full h-full px-5 py-7 flex-col gap-5">
+      <CalendarView />
+    </div>
+  );
 };
 
 export default Page;
