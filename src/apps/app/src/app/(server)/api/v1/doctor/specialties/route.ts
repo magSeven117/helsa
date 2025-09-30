e3522deff8a797dcb1e9c2980808843258b2c38d
magSeven117 <65859907+magSeven117@users.noreply.github.com>
2025-09-30 11:28:15 -0500

    Update route.ts

--- src/apps/app/src/app/(server)/api/v1/doctor/specialties/route.ts
+++ src/apps/app/src/app/(server)/api/v1/doctor/specialties/route.ts
@@ -5,7 +5,7 @@ import { GetSpecialties } from '@helsa/engine/doctor/application/services/get-sp
 import { PrismaDoctorRepository } from '@helsa/engine/doctor/infrastructure/persistence/prisma-doctor-repository';
 import { unstable_cache as cache } from 'next/cache';
 
-export const GET = routeHandler({ name: 'get-specialties' }, async ({ user }) => {
+export const GET = routeHandler({ name: 'get-specialties', authenticated: false }, async ({ user }) => {
   const service = new GetSpecialties(new PrismaDoctorRepository(database));
   const response = await cache(() => service.run(), ['get-specialties', user?.id.value ?? ''], {
     tags: [`get-specialties-${user?.id.value}`],
